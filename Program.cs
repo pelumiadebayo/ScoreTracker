087f59ecf3da8fe42d391db24e04b44c61295f78
Pelumi Adebayo <PAdebayo_Dev@polarisbanklimited.com>
2022-03-25 04:15:35 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ScoreTracker
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = {1,2,3,4,5};
+            ScoreTracker(array);
+        }
+
+        public static void ScoreTracker(int[] intArray)
+        {
+            int totalScore = 0;
+            foreach (var item in intArray)
+            {
+                if (item%2==0)
+                {
+                    totalScore += 1;
+                }
+                if (item % 2 > 0)
+                {
+                    totalScore += 3;
+                }
+                if (item==8)
+                {
+                    totalScore += 5;
+                }
+            }
+            Console.WriteLine(totalScore);
+        }
+    }
+}
